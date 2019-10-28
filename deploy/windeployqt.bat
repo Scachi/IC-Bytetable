c5966e21e857daac050c80e4247ae9440afa20f7
Scachi <22262242+Scachi@users.noreply.github.com>
2019-10-28 08:38:14 +0100

    lib folder problem
    Can't deploy with lib files in lib folder.
    Don't know how to tell the app to use the lib folder to look for the required files.
    Changed to putting libs into  the same folder where the .exe is for now.

--- deploy/windeployqt.bat
+++ deploy/windeployqt.bat
@@ -4,7 +4,6 @@ set PROJECTNAME=%1
 set APPVERSION=%2
 set OUTDIR=%PROJECTNAME%_%APPVERSION%
 rem windeployqt.exe %PROJECTNAME%.exe --release --dir %OUTDIR% --libdir %OUTDIR%\lib --no-plugins --no-quick-import --no-system-d3d-compiler --no-compiler-runtime --no-webkit2
-rem copy %PROJECTNAME%.exe %OUTDIR%\%PROJECTNAME%.exe
 copy %PROJECTNAME%.exe %OUTDIR%\%PROJECTNAME%.exe
-windeployqt.exe %OUTDIR% --libdir %OUTDIR%\lib --no-webkit2
+windeployqt.exe %OUTDIR%\%PROJECTNAME%.exe --release --dir %OUTDIR%
 rem pause
